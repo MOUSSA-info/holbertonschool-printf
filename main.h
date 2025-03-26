6519b8d52303e0e3885b9fa534ce155b43f9b06c
Trice97 <65173242+Trice97@users.noreply.github.com>
2025-03-26 21:56:24 +0100

    Update main.h

--- main.h
+++ main.h
@@ -2,13 +2,13 @@
 #define MAIN_H
 
 #include <stdarg.h>
-<<<<<<< HEAD
+
 
 int _printf(const char *format, ...);
 int _putchar(char c);
 
 #endif 
-=======
+
 #include <unistd.h>
 
 /* Prototype de la fonction principale*/
@@ -21,4 +21,4 @@ int _print_string(char *str);
 int print_percent(void);
 #endif /* MAIN_H */
 
->>>>>>> 67a1d29cd5046ce2bccb428a0cfd6248b3d3e431
+
